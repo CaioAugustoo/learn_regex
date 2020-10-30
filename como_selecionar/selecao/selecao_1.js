361674ddceeffec53df9fb7daf0f14ed07ac9569
Caio Augusto <caioamfr@gmail.com>
2020-10-29 21:13:58 -0300

    Adicionado novos métodos de seleção

--- como_selecionar/selecao/selecao_1.js
+++ como_selecionar/selecao/selecao_1.js
@@ -1,15 +1,38 @@
-// Procura: a
-const regex = /a/; // deve começar com as duas barras  "//"
+// // Procura: a
+// const regex = /a/; // deve começar com as duas barras  "//"
 
-const frase = "JavaScript";
-const fraseComRegex = frase.replace(regex, "b"); // substituimos na frase o que tiver "a" por "b"
+// const frase = "JavaScript";
+// const fraseComRegex = frase.replace(regex, "b"); // substituimos na frase o que tiver "a" por "b"
 
-console.log(fraseComRegex);
-// Resultado final: JbvaScript
+// console.log(fraseComRegex);
+// // Resultado final: JbvaScript
 
-// Utilizar um caracter literal irá realizar uma busca específica deste caracter
-// Procura: J seguido de "a", "v" e "a";
-const novoRegex = /Java/;
+// // Utilizar um caracter literal irá realizar uma busca específica deste caracter
+// // Procura: J seguido de "a", "v" e "a";
+// const novoRegex = /Java/;
 
-console.log("JavaScript".replace(novoRegex, "Type"));
-// Resultado final: TypeScript
+// console.log("JavaScript".replace(novoRegex, "Type"));
+// // Resultado final: TypeScript
+
+// // Character Class
+// // Quando quisermos procurar por determinadas letras, colocamos entre colchetes. Procura letra por letra, não a palavra inteira.
+
+// // Procura: Todo a, A, i, I
+// const novoRegex = /[ai]/gi;
+
+// console.log("JavaScript".replace(novoRegex, "u"));
+// // Resultado final: JuvuScrupt
+
+// // Para escapar um caracter, utilizamos a barra invertida
+// const novoRegex = /\./gi;
+
+// const frase =
+//   "RegExr was created by gskinner.com, and is proudly hosted by Media Temple.";
+// console.log(frase.replace(novoRegex, "'"));
+
+// // Resultado final: RegExr was created by gskinner'com, and is proudly hosted by Media Temple'
+
+// Para procurar de uma letra à outra, utilizamos hífen
+const novoRegex = /[a-z]/gi;
+
+console.log("Javascript é linguagem de programação".replace(novoRegex, "5"));
